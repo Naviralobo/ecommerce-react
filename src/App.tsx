3932840c5786105b794a5e3ffd9f3f29b9adfba6
Naviralobo <majel.josephine>
2026-04-23 17:05:34 +0530

    chore: install core dependencies and configure Tailwind CSS v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import "./App.css";
+import "./index.css";
 
 function App() {
-  return <div>Ecom app</div>;
+  return <div className="text-3xl font-bold text-blue-500">Ecom app</div>;
 }
 
 export default App;
