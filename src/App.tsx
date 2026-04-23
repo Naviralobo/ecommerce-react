ce20d35cb92c262ad8ddb13c1d507b50fa3df6b1
Naviralobo <majel.josephine>
2026-04-24 03:54:17 +0530

    feat: setup routing and create core application pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./index.css";
+import Home from "./pages/Home";
+import ProducDetails from "./pages/ProducDetails";
+import Cart from "./pages/Cart";
 
 function App() {
-  return <div className="text-3xl font-bold text-blue-500">Ecom app</div>;
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProducDetails />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/ProducDetails.tsx
@@ -0,0 +1,5 @@
+const ProducDetails = () => {
+  return <div>Product</div>;
+};
+
+export default ProducDetails;
