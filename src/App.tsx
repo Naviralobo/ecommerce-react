0a9b199c96882749f9721b35ea73318f5b7957b5
Naviralobo <majel.josephine>
2026-05-01 11:20:39 +0530

    feat: implement protected routes to restrict access to authenticated users

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import ProducDetails from "./pages/ProducDetails";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 
 import "./index.css";
 
@@ -16,7 +17,14 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
-        <Route path="/cart" element={<Cart />} />
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoute>
+              <Cart />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/" element={<Home />} />
 
         <Route path="/product/:id" element={<ProducDetails />} />
